d14f67faf8d6a18b3a9e4779028236283436eacb
Orlang00r <Orlangur00r@yandex.ru>
2022-04-18 17:39:00 +0400

    1 is ok

---
+++ Work 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+int max;
+Console.WriteLine("Input first integer number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second integer number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+
+    max = num1;
+
+else 
+    max = num2;
+
+Console.WriteLine("max number is " + max);
+
+*/
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
