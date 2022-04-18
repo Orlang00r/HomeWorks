77f251f49cd92df6a0755c6785d37807e05128cc
Orlang00r <Orlangur00r@yandex.ru>
2022-04-18 17:53:14 +0400

    3 is ok

--- Work 1/Program.cs
+++ Work 1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 /*
+
 int max;
 Console.WriteLine("Input first integer number");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -19,6 +20,8 @@ Console.WriteLine("max number is " + max);
 
 // Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+/*
+
 int max;
 Console.WriteLine("Input first integer number");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -36,3 +39,20 @@ if(num3>max)
     max=num3;
 
 Console.WriteLine("max number is " + max);
+
+*/
+
+// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.WriteLine("Input integer number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num % 2 == 0)
+    Console.WriteLine("even number");
+else 
+    Console.WriteLine("odd number");
+
+    */
+
+    
