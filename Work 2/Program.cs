ee3b19432f776f90035f14228c220977e2f5c486
Orlang00r <Orlangur00r@yandex.ru>
2022-04-21 19:35:50 +0400

    weekday 1

--- Work 2/Program.cs
+++ Work 2/Program.cs
@@ -19,5 +19,56 @@ if (num < 1000 & num > 99)
 }
 */
 
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+
+int Length(int num)
+{
+    int tmp = num;
+    while (tmp / 1000 > 0)
+    {
+        tmp = (tmp - (tmp % 10)) / 10;
+    }
+    tmp = tmp % 10;
+    return tmp;
+}
+
+Console.WriteLine("Input integer number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Length(num));
+
+*/
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+
+Console.WriteLine("Input number of weekday");
+int day = Convert.ToInt32(Console.ReadLine());
+
+void CorrectDay ()
+{
+    while (day<1 || day>7)
+        {
+        Console.WriteLine("Input correct number of weekday");
+        int day = Convert.ToInt32(Console.ReadLine());
+        }
+}  
+
+void DayOfWeek ()
+{
+        if(day>5)
+        {
+            Console.WriteLine("weekend");
+        }
+        else 
+            Console.WriteLine("weekday");
+}
+
+CorrectDay ();
+DayOfWeek();
+
 
 
