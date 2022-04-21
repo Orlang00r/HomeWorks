f825e86a6f74213a71321668047299f8e6f0dc1a
Orlang00r <Orlangur00r@yandex.ru>
2022-04-21 16:31:19 +0400

    search dozens corrected

--- Work 2/Program.cs
+++ Work 2/Program.cs
@@ -1,18 +1,23 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
 /*
+int Temp(int num)
+{
+    int tmp = ((num - (num / 100) * 100) - num % 10) / 10;
+    return tmp;
+}
+
 Console.WriteLine("Input three-digit integer number (-999 to 999)");
 int num = Convert.ToInt32(Console.ReadLine());
 
 if (num > -1000 & num < -99)
 {
-    int tmp = ((num - (num / 100) * 100) - num % 10) / 10;
-    Console.WriteLine(tmp);
+    Console.WriteLine(Temp(num));
 }
 if (num < 1000 & num > 99)
 {
-    int tmp = ((num - (num / 100) * 100) - num % 10) / 10;
-    Console.WriteLine(tmp);
+    Console.WriteLine(Temp(num));
 }
 */
+
+
 
