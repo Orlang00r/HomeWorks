9eb74e6e1faa9ec43c2d072e29f2dd87727a8dc0
Orlang00r <Orlangur00r@yandex.ru>
2022-04-25 12:25:59 +0400

    polyndrome

---
+++ Work 3/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+/*
+void poli(int n)
+{
+    int n1 = n / 10000;
+    int n2 = n % 10;
+    int n3 = n / 1000 - n1 * 10;
+    int n4 = (n % 100 - n2) / 10;
+    if (n1 == n2 && n3 == n4)
+    {    
+        Console.WriteLine("number is polyndrome");
+    }
+    else
+    {
+        Console.WriteLine("number is not polyndrome");
+    }
+}
+
+void CorrNum(int n0)
+{
+    while (n0 < 10000 || n0 >99999)
+    {
+        Console.WriteLine("Input number between 10000 & 99999");
+        n0 = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Input number between 10000 & 99999");
+int num = Convert.ToInt32(Console.ReadLine());
+
+CorrNum(num);
+poli(num);
+
+*/
+
