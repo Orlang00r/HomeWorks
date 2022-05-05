e957398741e0f554eb7235c9a34bd9b90387f2a4
Orlang00r <Orlangur00r@yandex.ru>
2022-05-05 13:15:49 +0400

    eegree finder recurs...

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -22,6 +22,8 @@ Console.WriteLine("Number in  binary system:  " + Sys2Metod(input, bin));
 
 // Подсчет вводимых элементов массива >0. 
 
+/*
+
 int[] IsArray(int[] array, int n, int input)
 {
     if (n == 0)
@@ -59,5 +61,33 @@ int N = 0;
 int[] MyArray = IsArray(arr, N, 1);
 Shov(MyArray);
 Console.WriteLine("Number of  elements much 0 is: " + Count(MyArray, 0, 0));
+
+*/
+
+
+// Программа проверяет, являтся ли первое число степенью второго.
+double Degree(int a, int b, int res)
+{
+    if ((a / b) * b == a) return Degree((a / b), b, res + 1);
+    return res + 1;
+}
+double result = -1;
+int res = -1;
+Console.WriteLine();
+Console.WriteLine("Программа настроена на положительные числа");
+Console.WriteLine();
+Console.WriteLine("Введите проверяемое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите проверочное число: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+deg = Math.Abs(deg);
+if (num == 1 && deg != 1) Console.WriteLine(num + " является " + 0 + " Степенью числа " + deg);
+if (num == 1 && deg == 1) Console.WriteLine(num + " является " + 1 + " Степенью числа " + deg);
+if (num != 1 && deg != 1) result = Degree(num, deg, res);
+if (result > 0) Console.WriteLine(num + " является " + result + " Степенью числа " + deg);
+else
+    if (num != 1) Console.WriteLine(num + " не является степенью числа " + deg);
+
 
 
