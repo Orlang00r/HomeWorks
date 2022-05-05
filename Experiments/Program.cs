b58ba435cc1755fe4a1cea7c23ad97b69a3be3c1
Orlang00r <Orlangur00r@yandex.ru>
2022-05-05 21:13:04 +0400

    Calc2 reworked

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -1,16 +1,11 @@
 ﻿// Cakc10=>2 recurs
 
-/*
+
 
 string Sys2Metod(int dex, string binary)
 {
-    if (dex <= 1)
-    {
-        string d = Convert.ToString(dex);
-        binary = d + binary;
-        return binary;
-    }
-    return Sys2Metod(dex / 2, Convert.ToString(dex - 2 * (dex / 2)) + binary);
+    if (dex >= 1) return Sys2Metod(dex / 2, Convert.ToString(dex%2) + binary);
+    return binary;
 }
 
 Console.WriteLine("Input DEX number: ");
@@ -18,7 +13,7 @@ int input = Convert.ToInt32(Console.ReadLine());
 string bin = "";
 Console.WriteLine("Number in  binary system:  " + Sys2Metod(input, bin));
 
-*/
+
 
 // Подсчет вводимых элементов массива >0. 
 
