6c5d9a73a5c6aa4281863c8897e7b43d5b612f46
Orlang00r <Orlangur00r@yandex.ru>
2022-05-06 15:29:47 +0400

    converter 2-16 all...

--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Cakc10=>2 recurs
 
-
-
+/*
 string Sys2Metod(int dex, string binary)
 {
     if (dex >= 1) return Sys2Metod(dex / 2, Convert.ToString(dex%2) + binary);
@@ -12,9 +11,11 @@ Console.WriteLine("Input DEX number: ");
 int input = Convert.ToInt32(Console.ReadLine());
 string bin = "";
 Console.WriteLine("Number in  binary system:  " + Sys2Metod(input, bin));
+*/
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Подсчет вводимых элементов массива >0. 
 
 /*
@@ -56,10 +57,11 @@ int N = 0;
 int[] MyArray = IsArray(arr, N, 1);
 Shov(MyArray);
 Console.WriteLine("Number of  elements much 0 is: " + Count(MyArray, 0, 0));
-
 */
 
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Программа проверяет, являтся ли первое число степенью второго.
 
 /*
@@ -87,6 +89,9 @@ else
     if (num != 1) Console.WriteLine(num + " не является степенью числа " + deg);
 */
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Расчет длины массива необходимого под перевод десятичного в довичное число.
 
 /*
@@ -103,6 +108,7 @@ Console.WriteLine(len);
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Программа позволяет получить код вводимого символа, если символ пустой, то выводит код пробела.
 // Метод реализован для символа, программа реализована, как защита от ввода пустого символа,что приводит к ошибкам.
 
@@ -121,6 +127,7 @@ if (str.Length>1) str = str.Substring(1);
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Метод для выведение символа по коду
 
 /*
@@ -131,3 +138,119 @@ char Str(int asc)
 }
 */
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Конвертер значений между системами от 2х до 16х
+
+int Ord(string smb)
+{
+    //Console.WriteLine(smb + "  "+smb);
+    int rez = 0;
+    rez = Char.ConvertToUtf32(smb, 0);
+    return rez;
+}
+
+char Str(int asc)
+{
+    char str = Convert.ToChar(asc);
+    return str;
+}
+
+string[] Arr(string str)
+{
+    string[] arr = new string[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        arr[i] = str[i].ToString();
+    }
+    return arr;
+}
+
+int MetodTo10x(string[] num, int i, int dex, int ind1)
+{
+    while (i < num.Length)
+    {
+        string curCh = num[i];
+        if (Ord(curCh) >= Ord("A"))
+        {
+            int zn = Ord(curCh) - Ord("A") + 10;
+            dex = dex + zn * Convert.ToInt32(Math.Pow(ind1, (num.Length - i) - 1));
+            return MetodTo10x(num, i + 1, dex, ind1);
+        }
+
+        else
+        {
+            int zn = Convert.ToInt32(curCh);
+            dex = dex + zn * Convert.ToInt32(Math.Pow(ind1, (num.Length - i) - 1));
+            return MetodTo10x(num, i + 1, dex, ind1);
+        }
+    }
+    return dex;
+}
+
+string EndVal(string endV, int dex1, int ind2)
+{
+    while (dex1 >=1)
+    {
+        int ost = dex1 % (ind2);
+        if (ost > 9)
+        {
+            endV = Str(Ord("A") - 10 + ost) + endV;
+        }
+        else
+        {
+            endV = ost.ToString() + endV;
+        }
+        return EndVal(endV, dex1 / ind2, ind2);
+    }
+    return endV;
+}
+
+Console.WriteLine("Введите индекс изначильный системы: 2 - 16: ");
+int index1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс конечной системы: 2 - 16:");
+int index2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для преобразования: ");
+string numIn = 0 + Convert.ToString(Console.ReadLine());
+
+string endVal = "";
+string[] numStr = Arr(numIn);
+int Ln = numStr.Length;
+int dex = MetodTo10x(numStr, 0, 0, index1);
+
+string result = EndVal(endVal, dex, index2);
+Console.WriteLine();
+Console.WriteLine(" Переведено в десятичную систему: " + dex);
+Console.WriteLine();
+Console.WriteLine(" Переведено в конечную систему: " + result);
+
+
+
+
+
+
+//////
+/*
+string Str(int asc)
+{
+    string str =  Convert.ToChar(asc);
+
+    return str;
+}
+
+int Ord(string smb)
+{
+    //Console.WriteLine(smb + "  "+smb);
+    int rez = 0;
+    rez = Char.ConvertToUtf32(smb, 0);
+    return rez;
+}
+Console.WriteLine("Введите символ для преобразования: ");
+string numIn = Convert.ToString(Console.ReadLine());
+
+int a = Ord(numIn);
+string s = Str(a);
+Console.WriteLine();
+Console.WriteLine(s);
+*/
